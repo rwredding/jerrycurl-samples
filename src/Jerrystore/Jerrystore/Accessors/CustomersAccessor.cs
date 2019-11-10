a2993a6cd9a00aa117e52adf2c380dbe66f42a8b
rhodosaur <lesnick@dancode.net>
2019-11-10 03:31:00 +0100

    add database view on spec

--- src/Jerrystore/Jerrystore/Accessors/CustomersAccessor.cs
+++ src/Jerrystore/Jerrystore/Accessors/CustomersAccessor.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Jerrycurl.Mvc;
+using Jerrystore.Views.Customers;
 
 namespace Jerrystore.Accessors
 {
     public class CustomersAccessor : Accessor
     {
-
+        public IList<CustomerDatabaseView> GetDatabase() => this.Query<CustomerDatabaseView>();
     }
 }
---
+++ src/Jerrystore/Jerrystore/Views/Customers/CustomerDatabaseView.cs
@@ -0,0 +1,24 @@
+﻿using Jerrystore.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jerrystore.Views.Customers
+{
+    public class CustomerDatabaseView : Customer
+    {
+        public IList<OrderView> Orders { get; set; }
+        public CustomerAddress Address { get; set; }
+
+        public class OrderView : Order
+        {
+            public IList<OrderLineView> Lines { get; set; }
+            public CustomerAddress Billing { get; set; }
+        }
+
+        public class OrderLineView : OrderLine
+        {
+            public Product Product { get; set; }
+        }
+    }
+}
