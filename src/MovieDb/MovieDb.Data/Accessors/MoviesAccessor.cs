6f06982f064fa2bf1a7f3298b128991d57fee6f3
rhodosaur <lesnick@dancode.net>
2020-01-14 02:10:47 +0100

    add a few accessors

---
+++ src/MovieDb/MovieDb.Data/Accessors/MoviesAccessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Jerrycurl.Mvc;
+
+namespace MovieDb.Data.Accessors
+{
+    public class MoviesAccessor : Accessor
+    {
+
+    }
+}
